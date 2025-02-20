a60d09ac7bff8284976d18560eedba0b37122a28
Camille Arbault <arbaultcamille@gmail.com>
2025-02-20 21:57:15 +0100

    experiment with river bends
    need to use a different center

--- src/map/drawMap.ts
+++ src/map/drawMap.ts
@@ -55,6 +55,7 @@ function drawRivers(
     ctx.beginPath();
     ctx.lineWidth = 2;
     let riverCell = region;
+    let riverBend = 0;
 
     while (
       riverCell &&
@@ -65,9 +66,33 @@ function drawRivers(
       ctx.beginPath();
       ctx.moveTo(riverCell.point.x, riverCell.point.y);
       const nextDown = regions[riverCell.downslope];
-      ctx.lineTo(nextDown.point.x, nextDown.point.y);
+
+      const center = {
+        x: (riverCell.point.x + nextDown.point.x) / 2,
+        y: (riverCell.point.y + nextDown.point.y) / 2,
+      };
+
+      const diffX = riverCell.point.x - center.x;
+      const diffY = riverCell.point.y - center.y;
+      const radius = Math.abs(
+        Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2))
+      );
+      const startAngle = Math.atan2(diffY, diffX);
+      const endAngle = Math.atan2(
+        nextDown.point.y - center.y,
+        nextDown.point.x - center.x
+      );
+      ctx.arc(
+        center.x,
+        center.y,
+        radius,
+        startAngle,
+        endAngle,
+        riverBend % 2 === 0 // alternate clockwise and counterclockwise bends
+      );
       ctx.stroke();
-      ctx.lineWidth = Math.min(MAX_RIVER_WIDTH, (ctx.lineWidth += 0.3));
+      riverBend++;
+      ctx.lineWidth = Math.min(MAX_RIVER_WIDTH, 2 + riverBend * 0.3);
       ctx.closePath();
       riverCell = nextDown;
     }
